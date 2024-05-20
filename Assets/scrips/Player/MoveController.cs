cfe1386cbfd45281d3b873d1983096ea24a89cac
Pablo <asdson5555@gmail.com>
2024-05-20 16:55:13 -0500

    move player

---
+++ Assets/scrips/Player/MoveController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] protected float maxSpeed = 15;
+    protected float distanceAtack = 20;
+    protected Transform Player;
+    protected Rigidbody rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        Player = GameObject.FindWithTag("Player").transform;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+
+    }
+    void Update()
+    {
+        Move();
+    }
+    protected virtual void Move()
+    {
+
+    }
+}
---
+++ Assets/scrips/Player/MovePlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MoveController
+{
+    [SerializeField] protected float rotationSpeed;
+    [SerializeField] float velocityUp = 5;
+    protected override void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput) * maxSpeed * Time.deltaTime;
+        Quaternion rotation = Quaternion.Euler(0, horizontalInput * rotationSpeed * Time.deltaTime, 0);
+        Vector3 movimiento = new Vector3(0, velocityUp * Time.deltaTime, 0);
+
+        rb.MovePosition(transform.position + transform.TransformDirection(movement));
+        rb.MoveRotation(rb.rotation * rotation);
+        rb.MovePosition(rb.position + movimiento);
+    }
+}
