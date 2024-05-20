67129ee7771bb00a9ece4648f78d47596a6b21c8
Pablo <asdson5555@gmail.com>
2024-05-20 18:29:54 -0500

    procedural a medias pipipi

--- Assets/scrips/Enemy/SpawnEnemy.cs
+++ Assets/scrips/Enemy/SpawnEnemy.cs
@@ -1,27 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class SpawnEnemy : MonoBehaviour
 {
     public GameObject[] Enemys;
     public Transform spawnPoint;
+    Transform Player;
     public float spawnDelay = 0;
-    public Transform Player;
-
+    public float timer = 0;
+    private void Start()
+    {
+        Player = GameObject.FindWithTag("Player").transform;
+    }
     void Update()
     {
         spawnDelay += Time.deltaTime;
+        timer += Time.deltaTime;
         if (Player != null && spawnDelay >= 1)
         {
-            spawnEnemy();
-            spawnDelay = 0;
+            if (timer / 2 == 0)
+            {
+                Instantiate(Enemys[0]);
+                spawnDelay = 0;
+            }
+            else if (timer / 2 == 1)
+            {
+                Instantiate(Enemys[1]);
+                spawnDelay = 0;
+            }
+            else if (timer >= 20)
+            {
+                Instantiate(Enemys[1]);
+                Instantiate(Enemys[0]);
+                timer = 0;
+            }
         }
-    }
-    void spawnEnemy()
-    {
-        int randomIndex = Random.Range(0, Enemys.Length);
-        GameObject enemyToSpawn = Enemys[randomIndex];
-        Instantiate(enemyToSpawn);
     }
 }
