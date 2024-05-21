92674dc03051bdfcd7951a6626ebcb4aabc3bd5e
Pablo <asdson5555@gmail.com>
2024-05-20 20:55:08 -0500

    procedural ahora si

--- Assets/scrips/Enemy/SpawnEnemy.cs
+++ Assets/scrips/Enemy/SpawnEnemy.cs
@@ -8,34 +8,42 @@ public class SpawnEnemy : MonoBehaviour
     public GameObject[] Enemys;
     public Transform spawnPoint;
     Transform Player;
-    public float spawnDelay = 0;
+    public float spawnDelay = 2;
     public float timer = 0;
+    public float timer2 = 0;
+    
     private void Start()
     {
         Player = GameObject.FindWithTag("Player").transform;
     }
     void Update()
     {
-        spawnDelay += Time.deltaTime;
         timer += Time.deltaTime;
-        if (Player != null && spawnDelay >= 1)
+        timer2 += Time.deltaTime;
+        if (Player!=null && timer>=spawnDelay)
         {
-            if (timer / 2 == 0)
-            {
-                Instantiate(Enemys[0]);
-                spawnDelay = 0;
-            }
-            else if (timer / 2 == 1)
-            {
-                Instantiate(Enemys[1]);
-                spawnDelay = 0;
-            }
-            else if (timer >= 20)
-            {
-                Instantiate(Enemys[1]);
-                Instantiate(Enemys[0]);
-                timer = 0;
-            }
+           spawing();
+           timer = 0;
+        }
+    }
+    void spawing()
+    { 
+        if(timer2 <= 10)
+        {
+            Instantiate(Enemys[0]);
+        }
+        else if(timer2 > 10 && timer2 <= 20)
+        {
+            Instantiate(Enemys[1]);
+        }
+        else if(timer2 >20 && timer2 <= 30)
+        {
+            Instantiate(Enemys[0]);
+            Instantiate(Enemys[1]);
+        }
+        else
+        {
+            timer2 = 0;
         }
     }
 }
